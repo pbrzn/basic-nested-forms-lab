73f2973fb88269a8c3c28151df528a7c7b97a690
Patrick Brennan <pbrennanmusic@gmail.com>
2021-08-07 15:06:37 -0400

    commit tree f37d279879ad3629a7c77ac300070f32692b0541

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,8 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
-    @recipe.ingredients << Ingredient.new
-    @recipe.ingredients << Ingredient.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
   def create
